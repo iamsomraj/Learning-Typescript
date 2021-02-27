ec455bd0354977d4483ecec79ea489fead64ac5b
Somraj Mukherjee <iamsomraj@gmail.com>
2021-02-27 21:54:47 +0530

    Added fetching json in typescript

---
+++ fetchjson/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const { id, title, completed } = todo;
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+    The todo with id: ${id}
+    has a title of: ${title}
+    Is it finished?: ${completed}
+  `);
+};
