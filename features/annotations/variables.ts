e51c402c1dd66daf75caa767c42f581e2962162a
Somraj Mukherjee <iamsomraj@gmail.com>
2021-02-27 23:35:20 +0530

    Added any type

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -25,3 +25,12 @@ let point: { x: number; y: number } = {
 const logMyNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use type annotations
+// 1. When function returns any type
+
+const json = '{ "x": 25, "y": 35}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// Start with 254
