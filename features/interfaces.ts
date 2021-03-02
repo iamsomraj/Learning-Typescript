283861d20c947cfd330dacdc86e3e653e5e76fbb
Somraj Mukherjee <iamsomraj@gmail.com>
2021-03-02 11:25:31 +0530

    Converted the interface into a more generic type

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,7 +1,35 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
+// interface Vehicle {
+//   name: string;
+//   year: Date;
+//   broken: boolean;
+//   summary(): string;
+// }
+
+// const oldCivic = {
+//   name: 'Honda Civic',
+//   year: new Date(),
+//   broken: true,
+//   summary(): string {
+//     return `
+//     Name: ${this.name}
+//     Year: ${this.year}
+//     Is it broken? ${this.broken}
+//     `;
+//   },
+// };
+
+// const printVehicle = (vehicle: Vehicle) => {
+//   console.log(vehicle.summary());
+// };
+
+// printVehicle(oldCivic);
+
+/**
+ *  Now we are making some changes to the interface.
+ *  We are making it more generic.
+ */
+
+interface Reportable {
   summary(): string;
 }
 
@@ -18,8 +46,8 @@ const oldCivic = {
   },
 };
 
-const printVehicle = (vehicle: Vehicle) => {
-  console.log(vehicle.summary());
+const printSummary = (item: Reportable) => {
+  console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
